e58ace55a1ae2fd7339f6dad1bd2b4f234fe061b
Jan Kammerath <jankammerath@googlemail.com>
2019-04-19 15:01:20 +0200

    Posting data works now, but still segfaults when posting empty string

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # CBCDB - Cryptographic Blockchain Database
 
-## copy header files from the container
+## Development information
+Development of cbcdb can be done inside the docker container
+
+### copy header files from the container
 Copy the header files like this to be able to use them on the container host:
 docker cp cbcdb:/usr/include/libconfig.h++ .header/libconfig.h++
--- src/class/HttpServer.cpp
+++ src/class/HttpServer.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <microhttpd.h>
+#include <cstring>
 using namespace std;
 #include "HttpServer.hpp"
 
 /* content type to use for http responses */
 #define CONTENT_TYPE "application/json"
+
+struct postStatus {
+    bool status;
+    char *buff;
+};
 
 HttpServer::HttpServer(int serverPort){
     this->port = serverPort;
@@ -12,7 +18,7 @@ HttpServer::HttpServer(int serverPort){
 
 /* starts the http server */
 void HttpServer::start(){
-    this->daemonHandle = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION,
+    this->daemonHandle = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY | MHD_USE_DEBUG,
 		       this->port, NULL, NULL, &HttpServer::handleRequest, NULL, MHD_OPTION_END);
 }
 
@@ -23,7 +29,51 @@ int HttpServer::handleRequest(void * cls, struct MHD_Connection * connection,
     int result;
     struct MHD_Response* response;
 
-    string jsonResult = "{\"status\":\"fail\"}";
+    /* convert method, url to string */
+    string urlString(url); 
+    string methodString(method);
+    string uploadData;
+
+    /* handle POST/PUT operations and theor upload data:
+        https://stackoverflow.com/questions/36416422/process-post-data-in-microhttp-server-in-c */
+    if(methodString != "GET" && ptr != NULL){
+        struct postStatus *post = NULL;
+        post = (struct postStatus*)*ptr;
+
+        /* this checks if post data was supplied behind the ptr */
+        if(post == NULL) {
+            post = (struct postStatus*)malloc(sizeof(struct postStatus));
+            post->status = false;
+            *ptr = post;
+        }
+
+        if(!post->status) {
+            post->status = true;
+            return MHD_YES;
+        } else {
+            if(*upload_data_size != 0) {
+                cout << "upload_data_size:" << std::to_string(*upload_data_size) << endl;
+                post->buff = (char*)malloc(*upload_data_size + 1);
+                strncpy(post->buff, upload_data, *upload_data_size);
+                post->buff[*upload_data_size] = 0;
+                *upload_data_size = 0;
+                return MHD_YES;
+            } else {
+                uploadData.assign(post->buff);
+                free(post->buff);
+            }
+        } 
+
+        /* free the post struct if still there */
+        if(post != NULL){
+            free(post); 
+        }
+    }
+
+    /* get size of upload and define default result */
+    string jsonResult = "{\"status\":\"fail\", \"url\": \"" + urlString 
+                        + "\", \"method\": \"" + methodString + "\", "
+                        + "\"uploadSize\": " + std::to_string(uploadData.size()) + "}";
 
     /* it is absolutely vital to use MHD_RESPMEM_MUST_COPY as otherwise memory exceptions will occur.
         See further information here: https://www.gnu.org/software/libmicrohttpd/manual/html_node/microhttpd_002dresponse-create.html */
@@ -31,6 +81,9 @@ int HttpServer::handleRequest(void * cls, struct MHD_Connection * connection,
     MHD_add_response_header (response, "Content-Type", CONTENT_TYPE);
     result = MHD_queue_response(connection, MHD_HTTP_OK, response);
     MHD_destroy_response(response);
+
+    /* clear context pointer */
+    *ptr = NULL; 
 
     return result;
 }
